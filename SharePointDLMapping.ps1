91ddfe63bd38ad9bd8b918c42b29a4d6c19b7d19
babyphatotia-lab <babyphatotia@gmail.com>
2025-12-28 09:48:14 -0500

    Update SharePointDLMapping.ps1

--- SharePointDLMapping.ps1
+++ SharePointDLMapping.ps1
@@ -1,4 +1,4 @@
-$URL = “https://your_domain.sharepoint.com/sites/test_site/Shared%20Documents” #<Replace with your document library URL copied in the first procedure> 
+--/$URL = “https://your_domain.sharepoint.com/sites/test_site/Shared%20Documents” #<Replace with your document library URL copied in the first procedure> 
 $IESession = Start-Process -file iexplore -arg $URL -PassThru -WindowStyle Hidden 
 Sleep 20 
 $IESession.Kill() 
